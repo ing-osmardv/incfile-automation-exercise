7ba946c97924659cbf671aafb12a7e2e8a6f6924
ing-osmardv <ing.osmardv@gmail.com>
2022-10-27 20:58:05 -0500

    feat: login to all page with environment variables

--- src/functions/set-tasks.js
+++ src/functions/set-tasks.js
@@ -5,6 +5,19 @@ export const setTasks = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(constants.URL);
-
+  await loginTodoPage(page);
   await browser.close();
+};
+
+const loginTodoPage = async (page) => {
+  try {
+    await page.type(constants.EMAIL_INPUT_SELECTOR, process.env.TODOIST_USER);
+    await page.type(constants.PASSWORD_INPUT_SELECTOR, process.env.TODOIST_PASSWORD);
+    await page.click(constants.LOGIN_BUTTON_SELECTOR);
+    await page.waitForNavigation({ waitUntil: "networkidle2" });
+  } catch (err) {
+    throw new Error(
+      "An error occurred while logging in. Please check your credentials"
+    );
+  }
 };
