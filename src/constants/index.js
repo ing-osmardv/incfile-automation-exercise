19e0288203c468848edc259364dee43fb4ca5448
ing-osmardv <ing.osmardv@gmail.com>
2022-10-27 20:28:32 -0500

    chore: add random list page constants

---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export * as randomList from './random-list';
---
+++ src/constants/random-list.js
@@ -0,0 +1,4 @@
+export const URL = 'https://randomtodolistgenerator.herokuapp.com/library';
+export const TASK_CARD_SELECTOR = ".taskCard";
+export const TITLE_SELECTOR = '.task-title div';
+export const DESCRIPTION_SELECTOR = '.card-text';
