ab0fba1576810d3525a1e00b8f0b15760de360bb
ing-osmardv <ing.osmardv@gmail.com>
2022-10-27 21:03:39 -0500

    feat: save the tasks on the page

--- src/functions/set-tasks.js
+++ src/functions/set-tasks.js
@@ -2,11 +2,12 @@ import puppeteer from "puppeteer";
 import { todoIst as constants } from "../constants";
 import { delay } from "../utils";
 
-export const setTasks = async () => {
+export const setTasks = async (tasks) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(constants.URL);
   await loginTodoPage(page);
+  await addTasks(page, tasks);
   await browser.close();
 };
 
@@ -22,4 +23,16 @@ const loginTodoPage = async (page) => {
       "An error occurred while logging in. Please check your credentials"
     );
   }
+};
+
+const addTasks = async (page, tasks) => {
+  await page.click(constants.ADD_BUTTON_SELECTOR);
+  for (const task of tasks) {
+    await page.type(constants.TASK_TITLE_SELECTOR, task.title);
+    await page.type(constants.TASK_DESCRIPTION_SELECTOR, task.description);
+    await page.click(constants.SAVE_TASK_BUTTON_SELECTOR);
+    await page.waitForResponse(constants.SAVE_TASK_API_URL);
+    console.info(`Task: '${task.title}' saved`);
+  }
+  await page.click(constants.CANCEL_TASK_BUTTON_SELECTOR);
 };
