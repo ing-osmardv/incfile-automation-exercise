b256912cfe578a43e9e7b0fec8269e4fac7d75ce
ing-osmardv <ing.osmardv@gmail.com>
2022-10-27 20:59:48 -0500

    feat: implement function to give extra time when logging in

--- src/functions/set-tasks.js
+++ src/functions/set-tasks.js
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer";
 import { todoIst as constants } from "../constants";
+import { delay } from "../utils";
 
 export const setTasks = async () => {
   const browser = await puppeteer.launch();
@@ -15,6 +16,7 @@ const loginTodoPage = async (page) => {
     await page.type(constants.PASSWORD_INPUT_SELECTOR, process.env.TODOIST_PASSWORD);
     await page.click(constants.LOGIN_BUTTON_SELECTOR);
     await page.waitForNavigation({ waitUntil: "networkidle2" });
+    await delay(250);
   } catch (err) {
     throw new Error(
       "An error occurred while logging in. Please check your credentials"
